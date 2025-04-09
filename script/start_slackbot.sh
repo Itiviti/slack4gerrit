002571d2c95c50de9d93a8c6665b5863fa606ab7
Benoit Corne <benoit.corne@itiviti.com>
2025-04-09 15:37:27 +0200

    Refactor the configuration to set tokens in env variables

--- script/start_slackbot.sh
+++ script/start_slackbot.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+export SLACK_BOT_TOKEN=<your bot token given by Slack>
+export SLACK_APP_TOKEN=<your app token given by Slack>
 java -classpath "./lib/*" com.ullink.slack.review.Connector 2> log.txt
